1714e9c222d0a30a0bb629f00ea0aec87c6d937b
adrianhrb <adrian.herrera.br@gmail.com>
2024-01-22 18:47:51 +0000

    Some problems solved

--- loaders/utils.py
+++ loaders/utils.py
@@ -9,8 +9,6 @@ def load_database_data(file, kind='M'):
     decoded_file = file.read().decode('utf-8').splitlines()
     reader = csv.DictReader(decoded_file)
     for row in reader:
-        if row['birthday'] != '':
-            date = datetime.strptime(row['birthday'], '%Y-%m-%d').date()
         if kind == 'P':
             if row['birthday'] != '':
                 date = datetime.strptime(row['birthday'], '%Y-%m-%d').date()
@@ -19,12 +17,14 @@ def load_database_data(file, kind='M'):
             )
         else:
             if row['date'] != '':
-                date = datetime.strptime(row['birthday'], '%Y-%m-%d').date()
+                date = datetime.strptime(row['date'], '%Y-%m-%d').date()
             Match.objects.create(
                 tournament=row['tournament'],
                 date=date,
                 round=row['round'],
-                duration=int(row['duration']),
+                duration=int(row['match_minutes'].split('.')[0]),
+                loser=0,
+                winner=0,
             )
     return print('Todo ok')
 
@@ -35,7 +35,7 @@ def assign_winners_and_losers(file):
     for row in reader:
         match = Match.objects.get(id=row['match_id'])
         player = Player.objects.get(id=row['player_id'])
-        if bool(row['winner']):
+        if row['winner'] == 'True':
             match.winner = player
         else:
             match.loser = player
