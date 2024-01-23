a434f5078e5e8be7def245c83ffb16dab5bb7283
adrianhrb <adrian.herrera.br@gmail.com>
2024-01-23 16:20:12 +0000

    Starting winner and loser api views

--- matches/api/views.py
+++ matches/api/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from matches.models import Match
+from django.shortcuts import get_object_or_404
 
 from .serializers import MatchSerializer
 
@@ -8,3 +12,7 @@ from .serializers import MatchSerializer
 class MatchViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
+
+class MatchWinnerView(APIView):
+    def get(self, request, pk, format=None):
+        winer = 
