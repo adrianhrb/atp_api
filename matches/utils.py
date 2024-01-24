235039301d78069d196221e526e023a4784dda57
adrianhrb <adrian.herrera.br@gmail.com>
2024-01-24 22:23:28 +0000

    Simulation of post request to api added

---
+++ matches/utils.py
@@ -0,0 +1,8 @@
+from .models import Match
+
+def build_custom_prompt(match: Match) -> str:
+    winner = match.winner.name
+    loser = match.loser.name
+    tournament = match.tournament
+    text = f'Make an overview of the last tennis match in {tournament}, where {winner} and {loser} played. The winner of the match was {winner} and loser was {loser}'
+    return text
--- matches/views.py
+++ matches/views.py
@@ -2,13 +2,51 @@ import requests
 
 from django.shortcuts import render
 from prettyconf import config
+
+from .utils import build_custom_prompt
 from .forms import MatchAI
 
 def form_ai(request):
     if request.method == 'POST':
         form = MatchAI(data=request.POST)
+        if form.is_valid():
+            url = config('URL')
+            data = {
+                'model': config('MODEL'),
+                'rol': 'user',
+                'message': build_custom_prompt(form.cleaned_data['match'])
+            }
+            # r = requests.post(url=url, data=data)
+            # response = r.json()
+            response = simulated_reponse(data)
+            generated_text = response['choices'][0]['message']['content']
+            return render(request, 'ai_result.html', dict(text=generated_text))
     else:
         form = MatchAI()
         return render(request, 'ai.html', dict(form=form))
 
-def process_request(request, )
+def simulated_reponse(data):
+    if data['model'] == config('MODEL') and data['rol'] == 'user':
+        response = {
+            'choices': [
+                {
+                    'finish_reason': 'length',
+                    'index': 0,
+                    'message': {
+                        'content': "El partido fue la bomba y la gente se lo gozó",
+                        'role': 'assistant'
+                    },
+                    'references': []
+                }
+            ],
+            'created': 170612544,
+            'id': 'foobar',
+            'model': 'minimal',
+            'object': 'text',
+            'ussage':{
+                'completion_tokens': 100,
+                'prompt_tokens': 35,
+                'total_tokens': 135
+            }
+        }
+        return response
