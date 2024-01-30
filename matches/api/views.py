e0d89c0c8849206db07880da15f2931be2f15c58
adrianhrb <adrian.herrera.br@gmail.com>
2024-01-30 15:18:24 +0000

    Views formated

--- matches/api/views.py
+++ matches/api/views.py
@@ -1,20 +1,19 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from matches.models import Match
+from players.api.serializers import PlayerSerializer
 from players.models import Player
 
-from django.shortcuts import get_object_or_404
-
 from .serializers import MatchSerializer
-from players.api.serializers import PlayerSerializer
 
 
 class MatchViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
+
 
 class MatchWinnerView(APIView):
     queryset = Match.objects.all()
@@ -24,7 +23,8 @@ class MatchWinnerView(APIView):
         player = get_object_or_404(Player, id=match.winner.id)
         serializer = PlayerSerializer(player)
         return Response(serializer.data)
-    
+
+
 class MatchLoserView(APIView):
     queryset = Match.objects.all()
 
@@ -32,6 +32,4 @@ class MatchLoserView(APIView):
         match = self.queryset.get(pk=pk)
         player = get_object_or_404(Player, id=match.loser.id)
         serializer = PlayerSerializer(player)
-        return Response(serializer.data)    
-    
-
+        return Response(serializer.data)
